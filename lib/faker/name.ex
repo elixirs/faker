878894df674dc875644890a1fc2cb2935850e818
Igor Kapkov <igasgeek@me.com>
2014-04-13 01:59:26 +0400

    refactor Faker.Name

--- lib/faker/name.ex
+++ lib/faker/name.ex
@@ -1,41 +1,40 @@
 defmodule Faker.Name do
   import Faker.Config, only: [locale: 0]
 
-  locale_file = "../../priv/locales/en.json"
-  locale_path = Path.expand(Path.join(__DIR__, locale_file))
-  {:ok, json} = File.read(locale_path)
+  data_path = Path.expand(Path.join(__DIR__, "../../priv/name.json"))
+  {:ok, json} = File.read(data_path)
   {:ok, json} = JSEX.decode(json, [{:labels, :binary}])
-  [{locale, modules}] = json
-  functions = Dict.get(HashDict.new(modules), "name")
-  Enum.each functions, fn({fun, list}) ->
-    defp unquote(binary_to_atom("#{fun}_count"))(unquote(binary_to_atom(locale))) do
-      unquote(Enum.count(list))
-    end
-    Enum.each Enum.with_index(list), fn({el, index}) ->
-      defp unquote(binary_to_atom("get_#{fun}"))(unquote(binary_to_atom(locale)), unquote(index+1)) do
-        unquote(el)
+  Enum.each json, fn({lang, functions}) ->
+    Enum.each functions, fn({fun, list}) ->
+      defp unquote(binary_to_atom("#{fun}_count"))(unquote(binary_to_atom(lang))) do
+        unquote(Enum.count(list))
+      end
+      Enum.each Enum.with_index(list), fn({el, index}) ->
+        defp unquote(binary_to_atom("get_#{fun}"))(unquote(binary_to_atom(lang)), unquote(index+1)) do
+          unquote(el)
+        end
       end
     end
   end
 
   def first_name do
-    get_first_name(locale, :crypto.rand_uniform(1, first_name_count(locale)))
+    get_first_name(locale, :crypto.rand_uniform(1, first_name_count(locale)+1))
   end
 
   def last_name do
-    get_last_name(locale, :crypto.rand_uniform(1, last_name_count(locale)))
+    get_last_name(locale, :crypto.rand_uniform(1, last_name_count(locale)+1))
   end
 
   def name do
-    name(:crypto.rand_uniform(1, 10))
+    name(:crypto.rand_uniform(1, 11))
   end
 
   def prefix do
-    get_prefix(locale, :crypto.rand_uniform(1, prefix_count(locale)))
+    get_prefix(locale, :crypto.rand_uniform(1, prefix_count(locale)+1))
   end
 
   def suffix do
-    get_suffix(locale, :crypto.rand_uniform(1, suffix_count(locale)))
+    get_suffix(locale, :crypto.rand_uniform(1, suffix_count(locale)+1))
   end
 
   def title do
@@ -59,14 +58,14 @@ defmodule Faker.Name do
   end
 
   defp title_descriptor do
-    get_title_descriptor(locale, :crypto.rand_uniform(1, title_descriptor_count(locale)))
+    get_title_descriptor(locale, :crypto.rand_uniform(1, title_descriptor_count(locale)+1))
   end
 
   defp title_level do
-    get_title_level(locale, :crypto.rand_uniform(1, title_level_count(locale)))
+    get_title_level(locale, :crypto.rand_uniform(1, title_level_count(locale)+1))
   end
 
   defp title_job do
-    get_title_job(locale, :crypto.rand_uniform(1, title_job_count(locale)))
+    get_title_job(locale, :crypto.rand_uniform(1, title_job_count(locale)+1))
   end
 end
