94135be8c3a520d77d40ebde87ae032d6d69d1f8
Igor Kapkov <igasgeek@me.com>
2014-06-12 16:09:05 +0400

    Add tests for Faker.Address

---
+++ test/faker/address_test.exs
@@ -0,0 +1,79 @@
+defmodule AddressTest do
+  use ExUnit.Case, async: true
+
+  test "building_number/0" do
+    assert is_binary(Faker.Address.building_number)
+    assert size(Faker.Address.building_number) in 1..5
+  end
+
+  test "city/0" do
+    assert is_binary(Faker.Address.city)
+  end
+
+  test "city_prefix/0" do
+    assert is_binary(Faker.Address.city_prefix)
+  end
+
+  test "city_suffix/0" do
+    assert is_binary(Faker.Address.city_suffix)
+  end
+
+  test "country/0" do
+    assert is_binary(Faker.Address.country)
+  end
+
+  test "latitude/0" do
+    assert is_float(Faker.Address.latitude)
+    assert Faker.Address.latitude in -90..90
+  end
+
+  test "longitude/0" do
+    assert is_float(Faker.Address.longitude)
+    assert Faker.Address.longitude in -180..180
+  end
+
+  test "postcode/0" do
+    assert is_binary(Faker.Address.postcode)
+  end
+
+  test "secondary_address/0" do
+    assert is_binary(Faker.Address.secondary_address)
+  end
+
+  test "state/0" do
+    assert is_binary(Faker.Address.state)
+  end
+
+  test "state_abbr/0" do
+    assert is_binary(Faker.Address.state_abbr)
+  end
+
+  test "street_address/0" do
+    assert is_binary(Faker.Address.street_address)
+  end
+
+  test "street_address/1" do
+    assert is_binary(Faker.Address.street_address(true))
+    assert is_binary(Faker.Address.street_address(false))
+  end
+
+  test "street_name/0" do
+    assert is_binary(Faker.Address.street_name)
+  end
+
+  test "street_suffix/0" do
+    assert is_binary(Faker.Address.street_suffix)
+  end
+
+  test "time_zone/0" do
+    assert is_binary(Faker.Address.time_zone)
+  end
+
+  test "zip/0" do
+    assert is_binary(Faker.Address.zip)
+  end
+
+  test "zip_code/0" do
+    assert is_binary(Faker.Address.zip_code)
+  end
+end
