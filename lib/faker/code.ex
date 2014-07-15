b78d27eb0759a233a56e925a58bfd2074aa1482e
Vova <v@vforvova.com>
2014-07-15 16:08:50 +0400

    Add checksum calculation for isbn10

--- lib/faker/code.ex
+++ lib/faker/code.ex
@@ -3,7 +3,8 @@ defmodule Faker.Code do
 
   def isbn10 do
     :random.seed(:erlang.now)
-    Faker.format("#########") <> hd(Enum.shuffle(["#", "X"]))
+    sequence = Faker.format("#########")
+    sequence <> last_digit(sequence <> "0")
   end
 
   def isbn13 do
@@ -14,5 +15,37 @@ defmodule Faker.Code do
   def issn do
     :random.seed(:erlang.now)
     Faker.format("####-###") <> hd(Enum.shuffle(["#", "X"]))
+  end
+
+  def last_digit(sequence) do
+    mult = fn({e, i}) -> digit(e) * (i + 1) end
+
+    graphemes = String.graphemes(sequence)
+    reversed_graphemes_with_index = Enum.reverse(graphemes) |> Stream.with_index
+    checksum = reversed_graphemes_with_index |> Stream.map(mult) |> Enum.sum
+    rem = rem(checksum, 11)
+    if rem == 0 do
+      "0"
+    else
+      d = 11 - rem
+      digit_to_code_binary d
+    end
+  end
+
+  defp digit_to_code_binary(10) do
+    "X"
+  end
+
+  defp digit_to_code_binary(digit) do
+    Integer.to_string(digit)
+  end
+
+  def digit("X") do
+    10
+  end
+
+  def digit(str) when str !== "X" do
+    {1, digit} = {String.length(str), String.to_integer(str)}
+    digit
   end
 end
--- test/faker/code_test.exs
+++ test/faker/code_test.exs
@@ -1,14 +1,34 @@
 defmodule CodeTest do
   use ExUnit.Case, async: true
 
+  defp reverse_codepoints_with_index(str) do
+    String.codepoints(str) |> Enum.reverse |> Stream.with_index
+  end
+
+  defp digit("X") do
+    10
+  end
+
+  defp digit(str) when str !== "X" do
+    {1, digit} = {String.length(str), String.to_integer(str)}
+    digit
+  end
+
+  defp checksum10(isbn) do
+    mult = fn({e, i}) -> digit(e) * (i + 1) end
+    reverse_codepoints_with_index(isbn) |> Stream.map(mult) |> Enum.sum
+  end
+
   test "isbn/0" do
     assert is_binary(Faker.Code.isbn)
     assert String.length(Faker.Code.isbn) == 10
+    assert rem(checksum10(Faker.Code.isbn), 11) == 0
   end
 
   test "isbn10/0" do
     assert is_binary(Faker.Code.isbn10)
     assert String.length(Faker.Code.isbn10) == 10
+    assert rem(checksum10(Faker.Code.isbn10), 11) == 0
   end
 
   test "isbn13/0" do
