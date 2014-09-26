ba6eca49414bab9b00d0e57854be6cd62cb0dc6e
Tres Trantham <tres@trestrantham.com>
2014-09-26 16:28:09 -0400

    Fix test names

--- test/faker/name_test.exs
+++ test/faker/name_test.exs
@@ -1,11 +1,27 @@
-defmodule TeamTest do
+defmodule NameTest do
   use ExUnit.Case, async: true
 
-  test "creature/0" do
-    assert is_binary(Faker.Team.creature)
+  test "first_name/0" do
+    assert is_binary(Faker.Name.first_name)
+  end
+
+  test "last_name/0" do
+    assert is_binary(Faker.Name.last_name)
   end
 
   test "name/0" do
-    assert is_binary(Faker.Team.name)
+    assert is_binary(Faker.Name.name)
+  end
+
+  test "prefix/0" do
+    assert is_binary(Faker.Name.prefix)
+  end
+
+  test "suffix/0" do
+    assert is_binary(Faker.Name.suffix)
+  end
+
+  test "title/0" do
+    assert is_binary(Faker.Name.title)
   end
 end
