55e3f57040c352a484bb6b4df1c2bb42091b6e58
Andrea Leopardi <an.leopardi@gmail.com>
2015-01-05 22:32:22 +0100

    Minor 'typesetting' edits to mix.exs

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Faker.Mixfile do
       app: :faker,
       version: "0.4.1",
       elixir: "~> 1.0.0",
-      description: "Faker is pure Elixir library for generating fake data.",
+      description: "Faker is a pure Elixir library for generating fake data.",
       package: package,
       deps: deps
     ]
@@ -30,7 +30,7 @@ defmodule Faker.Mixfile do
       files: ["lib", "priv", "mix.exs", "mix.lock"],
       contributors: ["Igor Kapkov"],
       licenses: ["MIT"],
-      links: %{"Github" => "https://github.com/igas/faker"}
+      links: %{"GitHub" => "https://github.com/igas/faker"}
     ]
   end
 end
