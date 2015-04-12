8aeb5cd017092be0f9a771d21a8879afd77f1727
Igor Kapkov <igasgeek@me.com>
2015-04-12 14:37:12 +0800

    Bump 0.5.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Faker.Mixfile do
   def project do
     [
       app: :faker,
-      version: "0.5.0",
+      version: "0.5.1",
       elixir: "~> 1.0.0",
       description: "Faker is a pure Elixir library for generating fake data.",
       package: package,
