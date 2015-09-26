69c04462e2420201e4cb3273a2cab79cb1350138
Igor Kapkov <igasgeek@me.com>
2015-09-26 19:39:30 +1000

    Add missing Faker.Color.rgb_decimal/0 test

--- test/faker/color_test.exs
+++ test/faker/color_test.exs
@@ -2,7 +2,15 @@ defmodule Faker.ColorTest do
   use ExUnit.Case, async: true
   import Faker.Color
 
-  test :rgb_hex do
+  test "rgb_decimal/0" do
+    assert tuple_size(rgb_decimal) == 3
+    0..100 |> Enum.each(fn(_) ->
+      assert rgb_decimal |> Tuple.to_list |> Enum.all?(&(&1 >= 0))
+      assert rgb_decimal |> Tuple.to_list |> Enum.all?(&(&1 <= 255))
+    end)
+  end
+
+  test "rgb_hex/0" do
     assert String.length(rgb_hex) == 6
     assert Regex.match?(~r/^[0-9A-F]{6}$/, rgb_hex)
   end
