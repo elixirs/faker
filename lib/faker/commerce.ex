facb1809976b02c4ab3eafcb4ae121ea2960fcb1
Saúl Cabrera <saulecabrera@gmail.com>
2015-09-27 18:17:08 -0400

    refactored commerce module according to discussion on issue #12

--- lib/faker/commerce.ex
+++ lib/faker/commerce.ex
@@ -1,55 +1,61 @@
 defmodule Faker.Commerce do
-  data_path = Path.expand(Path.join(__DIR__, "../../priv/commerce.json"))
-  json = File.read!(data_path) |> Poison.Parser.parse!
-  Enum.each json, fn(el) ->
-    {lang, data} = el
-    Enum.each data, fn
-      {"values", values} ->
-        Enum.each values, fn({fun, list}) ->
-          def unquote(String.to_atom(fun))() do
-            unquote(String.to_atom("get_#{fun}"))(Faker.locale, :crypto.rand_uniform(0, unquote(String.to_atom("#{fun}_count"))(Faker.locale)))
-          end
-          defp unquote(String.to_atom("#{fun}_count"))(unquote(String.to_atom(lang))) do
-            unquote(Enum.count(list))
-          end
-          Enum.with_index(list) |> Enum.each fn({el, index}) ->
-            defp unquote(String.to_atom("get_#{fun}"))(unquote(String.to_atom(lang)), unquote(index)) do
-              unquote(el)
-            end
-          end
-        end
-      {"formats", values} ->
-        Enum.each values, fn({fun, list}) ->
-          def unquote(String.to_atom(fun))() do
-            unquote(String.to_atom("format_#{fun}"))(Faker.locale, :crypto.rand_uniform(0, unquote(String.to_atom("#{fun}_count"))(Faker.locale)))
-          end
-          Enum.with_index(list) |> Enum.each fn({el, index}) ->
-            defp unquote(String.to_atom("format_#{fun}"))(unquote(String.to_atom(lang)), unquote(index)) do
-              Faker.format(unquote(el))
-            end
-          end
-          defp unquote(String.to_atom("#{fun}_count"))(unquote(String.to_atom(lang))) do
-            unquote(Enum.count(list))
-          end
-        end
-      {"functions", values} ->
-        Enum.each values, fn({fun, list}) ->
-          def unquote(String.to_atom(fun))() do
-            unquote(String.to_atom(fun))(Faker.locale, :crypto.rand_uniform(0, unquote(String.to_atom("#{fun}_count"))(Faker.locale)))
-          end
-          Enum.with_index(list) |> Enum.each fn({el, index}) ->
-            defp unquote(String.to_atom(fun))(unquote(String.to_atom(lang)), unquote(index)) do
-              unquote(Code.string_to_quoted!('"#{el}"'))
-            end
-          end
-          defp unquote(String.to_atom("#{fun}_count"))(unquote(String.to_atom(lang))) do
-            unquote(Enum.count(list))
-          end
-        end
-    end
+  @moduledoc """
+  Functions for generating commerce related data
+  """
+
+  @doc """
+  Return a random color
+  """
+  @spec color() :: String.t
+  def color do 
+    Module.concat(__MODULE__, Faker.mlocale).color
+  end
+  
+  @doc """
+  Return a random department
+  """
+  @spec department() :: String.t
+  def department do
+    Module.concat(__MODULE__, Faker.mlocale).department
   end
 
-  def price do
-    :crypto.rand_uniform(1, 10001) / 100.0
+  @doc """
+  Return a random number that represents a price
+  """
+  @spec price() :: float
+  def price do 
+    :crypto.rand_uniform(1, 1001) / 100.0
+  end
+  
+  @doc """
+  Returns a complete product name, based on product adjectives, product materials, product names
+  """
+  @spec product_name() :: String.t
+  def product_name do 
+    Module.concat(__MODULE__, Faker.mlocale).product_name
+  end
+
+  @doc """
+  Return a random adjective for a product
+  """
+  @spec product_name_adjective() :: String.t
+  def product_name_adjective do 
+    Module.concat(__MODULE__, Faker.mlocale).product_name_adjective
+  end
+
+  @doc """
+  Return a random product material
+  """
+  @spec product_name_material() :: String.t
+  def product_name_material do 
+    Module.concat(__MODULE__, Faker.mlocale).product_name_material  
+  end
+
+  @doc """
+  Return a random product name
+  """
+  @spec product_name_product() :: String.t
+  def product_name_product do 
+    Module.concat(__MODULE__, Faker.mlocale).product_name_product
   end
 end
