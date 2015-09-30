e70fba08b7f88e704372e357d7b1c8e29e95376e
Saúl Cabrera <saulecabrera@gmail.com>
2015-09-30 13:44:09 -0400

    Team module refactoring

---
+++ lib/faker/team/en.ex
@@ -0,0 +1,21 @@
+defmodule Faker.Team.En do 
+  @moduledoc """
+  Functions for generating team related data in English
+  """
+
+  @doc """
+  Return a string of the form [state] [creature]
+  """
+  @spec name() :: String.t
+  def name, do: "#{Faker.Address.state} #{creature}"
+
+  @data ["ants", "banshees", "bats", "bears", "bees", "birds", "black cats", "buffalo", "cats", "cattle", "chickens", "chimeras", "conspirators", "crows", "dogs", "dolphins", "dragons", "druids", "ducks", "dwarves", "elephants", "elves", "enchanters", "exorcists", "fishes", "foes", "foxes", "frogs", "geese", "ghosts", "giants", "gnomes", "goats", "goblins", "gooses", "griffins", "horses", "kangaroos", "lions", "lycanthropes", "monkeys", "nemesis", "ogres", "oracles", "owls", "oxen", "penguins", "people", "pigs", "prophets", "rabbits", "sheep", "sons", "sorcerors", "spiders", "spirits", "tigers", "vampires", "vixens", "warlocks", "werewolves", "whales", "witches", "wolves", "worshipers", "zebras", "zombies"]
+  @data_count Enum.count(@data)
+  @doc """
+  Return a random creature name
+  """
+  @spec creature() :: String.t
+  def creature do 
+    Enum.at(@data, :crypto.rand_uniform(0, @data_count))
+  end
+end
