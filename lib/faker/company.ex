94c56d7fd7b4dffda186266e05b499206b051a64
Vova <v@vforvova.ru>
2015-10-03 15:59:12 +0300

    [#25] Get rid of json programming in Faker.Company

--- lib/faker/company.ex
+++ lib/faker/company.ex
@@ -1,37 +1,85 @@
 defmodule Faker.Company do
-  data_path = Path.expand(Path.join(__DIR__, "../../priv/company.json"))
-  json = File.read!(data_path) |> Poison.Parser.parse!
-  Enum.each json, fn(el) ->
-    {lang, data} = el
-    Enum.each data, fn
-      {"values", values} ->
-        Enum.each values, fn({fun, list}) ->
-          def unquote(String.to_atom(fun))() do
-            unquote(String.to_atom("get_#{fun}"))(Faker.locale, :crypto.rand_uniform(0, unquote(String.to_atom("#{fun}_count"))(Faker.locale)))
-          end
-          defp unquote(String.to_atom("#{fun}_count"))(unquote(String.to_atom(lang))) do
-            unquote(Enum.count(list))
-          end
-          Enum.with_index(list) |> Enum.each fn({el, index}) ->
-            defp unquote(String.to_atom("get_#{fun}"))(unquote(String.to_atom(lang)), unquote(index)) do
-              unquote(el)
-            end
-          end
-        end
-      {"functions", values} ->
-        Enum.each values, fn({fun, list}) ->
-          def unquote(String.to_atom(fun))() do
-            unquote(String.to_atom(fun))(Faker.locale, :crypto.rand_uniform(0, unquote(String.to_atom("#{fun}_count"))(Faker.locale)))
-          end
-          Enum.with_index(list) |> Enum.each fn({el, index}) ->
-            defp unquote(String.to_atom(fun))(unquote(String.to_atom(lang)), unquote(index)) do
-              unquote(Code.string_to_quoted!('"#{el}"'))
-            end
-          end
-          defp unquote(String.to_atom("#{fun}_count"))(unquote(String.to_atom(lang))) do
-            unquote(Enum.count(list))
-          end
-        end
-    end
+  @moduledoc """
+  Functions for generating company names and related stuff
+  """
+
+  @doc """
+  Returns a random complete business related bullshit
+  """
+  @spec bs() :: String.t
+  def bs do
+    Module.concat(__MODULE__, Faker.mlocale).bs
+  end
+
+  @doc """
+  Returns a random business related bullshit
+  """
+  @spec bullshit() :: String.t
+  def bullshit do
+    Module.concat(__MODULE__, Faker.mlocale).bullshit
+  end
+
+  @doc """
+  Returns a random business related bullshit prefix
+  """
+  @spec bullshit_prefix() :: String.t
+  def bullshit_prefix do
+    Module.concat(__MODULE__, Faker.mlocale).bullshit_prefix
+  end
+
+  @doc """
+  Returns a random business related bullshit suffix
+  """
+  @spec bullshit_suffix() :: String.t
+  def bullshit_suffix do
+    Module.concat(__MODULE__, Faker.mlocale).bullshit_suffix
+  end
+
+  @doc """
+  Returns a random business related buzzword
+  """
+  @spec buzzword() :: String.t
+  def buzzword do
+    Module.concat(__MODULE__, Faker.mlocale).buzzword
+  end
+
+  @doc """
+  Returns a random business related buzzword prefix
+  """
+  @spec buzzword_prefix() :: String.t
+  def buzzword_prefix do
+    Module.concat(__MODULE__, Faker.mlocale).buzzword_prefix
+  end
+
+  @doc """
+  Returns a random business related buzzword suffix
+  """
+  @spec buzzword_suffix() :: String.t
+  def buzzword_suffix do
+    Module.concat(__MODULE__, Faker.mlocale).buzzword_suffix
+  end
+
+  @doc """
+  Returns a random complete catch phrase
+  """
+  @spec catch_phrase() :: String.t
+  def catch_phrase do
+    Module.concat(__MODULE__, Faker.mlocale).catch_phrase
+  end
+
+  @doc """
+  Returns complete company name
+  """
+  @spec name() :: String.t
+  def name do
+    Module.concat(__MODULE__, Faker.mlocale).name
+  end
+
+  @doc """
+  Returns a random type of business entity
+  """
+  @spec suffix() :: String.t
+  def suffix do
+    Module.concat(__MODULE__, Faker.mlocale).suffix
   end
 end
