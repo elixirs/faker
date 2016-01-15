eebe70fcae7ef29fa5deae6ebbaa02060bc2322f
Igor Kapkov <igasgeek@me.com>
2016-01-15 22:51:24 +1100

    Update mix config

--- mix.exs
+++ mix.exs
@@ -1,39 +1,33 @@
 defmodule Faker.Mixfile do
   use Mix.Project
 
+  @version "0.6.0"
+
   def project do
-    [
-      app: :faker,
-      version: "0.6.0",
-      elixir: "~> 1.0",
-      description: "Faker is a pure Elixir library for generating fake data.",
-      package: package,
-      name: "Faker",
-      source_url: "https://github.com/igas/faker",
-      deps: deps
-    ]
+    [app: :faker,
+     version: @version,
+     elixir: "~> 1.0",
+     description: "Faker is a pure Elixir library for generating fake data.",
+     package: package,
+     name: "Faker",
+     source_url: "https://github.com/igas/faker",
+     deps: deps]
   end
 
   def application do
-    [
-      applications: [:crypto],
-      env: [locale: :en]
-    ]
+    [applications: [:crypto],
+     env: [locale: :en]]
   end
 
   defp deps do
-    [
-      {:ex_doc, "~> 0.7", only: :dev},
-      {:earmark, ">= 0.0.0", only: :dev}
-    ]
+    [{:ex_doc, "~> 0.7", only: :docs},
+     {:earmark, ">= 0.0.0", only: :docs}]
   end
 
   defp package do
-    [
-      files: ["lib", "mix.exs", "mix.lock"],
+    %{files: ["lib", "mix.exs", "mix.lock"],
       maintainers: ["Igor Kapkov"],
       licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/igas/faker"}
-    ]
+      links: %{"GitHub" => "https://github.com/igas/faker"}}
   end
 end
