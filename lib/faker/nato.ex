bd7281a584cab3e905b3de4597ba5f115c085b93
Pete Hamilton <peterejhamilton@gmail.com>
2016-07-23 14:06:51 +0100

    Support NATO phoentic alphabet
    Adds a module for generating strings from the nato phonetic alphabet.
    Useful for generating categories/severity levels/team names etc.
    
    Examples
    ---------
    
        # Letters
        Faker.Nato.letter_code_word => "ALPHA"
        Faker.Nato.letter_code_word => "FOXTROT"
    
        # Digits
        Faker.Nato.digit_code_word => "ONE"
        Faker.Nato.digit_code_word => "THREE"
    
        # Formatting
        Faker.Nato.format("?-?-?") => "ALPHA CHARLIE ZULU"
    
        # Easy Callsigns
        Faker.Nato.callsign => "DELTA XRAY FIVE"
    
    Phoentics taken from https://en.wikipedia.org/wiki/NATO_phonetic_alphabet

---
+++ lib/faker/nato.ex
@@ -0,0 +1,62 @@
+defmodule Faker.Nato do
+  import Faker, only: [sampler: 2]
+
+  @moduledoc """
+  Functions for generating NATO alphabet data
+  """
+
+  @doc """
+  Returns a random letter NATO code
+  """
+  @spec letter_code_word :: String.t
+  sampler :letter_code_word, ["ALPHA", "BRAVO", "CHARLIE", "DELTA", "ECHO", "FOXTROT", "GOLF", "HOTEL", "INDIA", "JULIETT", "KILO", "LIMA", "MIKE", "NOVEMBER", "OSCAR", "PAPA", "QUEBEC", "ROMEO", "SIERRA", "TANGO", "UNIFORM", "VICTOR", "WHISKEY", "XRAY", "YANKEE", "ZULU"]
+
+  @doc """
+  Returns a random digit NATO code
+  """
+  @spec digit_code_word :: String.t
+  sampler :digit_code_word, ["ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "ZERO"]
+
+  @doc """
+  Returns the NATO stop code
+  """
+  @spec stop_code_word :: String.t
+  def stop_code_word, do: "STOP"
+
+  @doc """
+  Returns a random NATO callsign of the form [alpha]-[alpha]-[digit]
+  """
+  @spec callsign() :: String.t
+  def callsign, do: format("?-?-#")
+
+  @doc """
+  Formats a string using the NATO alphabet.
+
+  It replaces `"#"` to a random NATO digit, `"?"` to random NATO letter
+  and `"."` to the stop code.
+  """
+  @spec format(String.t) :: String.t
+  def format(str) when is_binary(str) do
+    format(str, "")
+  end
+
+  defp format(<<"#" :: utf8, tail :: binary>>, acc) do
+    format(tail, <<acc :: binary, "#{digit_code_word}">>)
+  end
+
+  defp format(<<"?" :: utf8, tail :: binary>>, acc) do
+    format(tail, <<acc :: binary, "#{letter_code_word}">>)
+  end
+
+  defp format(<<"." :: utf8, tail :: binary>>, acc) do
+    format(tail, <<acc :: binary, "#{stop_code_word}">>)
+  end
+
+  defp format(<<other :: utf8, tail :: binary>>, acc) do
+    format(tail, <<acc :: binary, other>>)
+  end
+
+  defp format("", acc) do
+    acc
+  end
+end
