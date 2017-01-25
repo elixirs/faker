0feb1894df3c8f09e813ea0580be8308fc6fe1db
Wojtek Mach <wojtekmach@users.noreply.github.com>
2017-01-25 09:18:33 +0100

    Add Faker.App.semver (#76)
    * Add App.semver
    
    * Update CHANGELOG & USAGE
    
    * Fix typespec
    
    * Change defaults

--- USAGE.md
+++ USAGE.md
@@ -67,7 +67,11 @@ Faker.App.author #=> "Angie Graham IV"
 
 Faker.App.name #=> "Fixflex"
 
-Faker.App.version #=> "4.3.3"
+Faker.App.version #=> "4.3"
+
+Faker.App.semver #=> "0.1.6"
+
+Faker.App.semver(allow_pre: true, allow_build: true) #=> "1.0.0-rc.1+001"
 ```
 
 ### Faker.Avatar
--- lib/faker/app.ex
+++ lib/faker/app.ex
@@ -9,7 +9,7 @@ defmodule Faker.App do
   """
 
   @doc """
-  Return random version number.
+  Returns a version number.
   """
   @spec version() :: String.t
   def version do
@@ -19,13 +19,41 @@ defmodule Faker.App do
   end
 
   @doc """
-  Return app name.
+  Returns a SemVer version.
+
+  ## Options:
+
+  * `:allow_pre` - when `true`, a pre-release identifier (e.g.: `-dev`)
+    will be appended (default: `false`)
+  * `:allow_build` - when `true`, a build identifier (e.g.: `+001`)
+    will be appended (default: `false`)
+  """
+  @spec semver(Keyword.t) :: String.t
+  def semver(opts \\ []) do
+    allow_pre = Keyword.get(opts, :allow_pre, false)
+    allow_build = Keyword.get(opts, :allow_build, false)
+    pre =
+      ~w(dev alpha beta rc.0 rc.1)
+      |> Enum.at(:crypto.rand_uniform(0, 5))
+
+    formats = ["0.#.#", "#.#.#", "#.##.##"]
+    formats = if allow_pre, do: formats ++ ["#.#.#-#{pre}"], else: formats
+    formats = if allow_build, do: formats ++ ["#.#.#+###"], else: formats
+    formats = if allow_pre and allow_build, do: formats ++ ["#.#.#-#{pre}+###"], else: formats
+
+    formats
+    |> Enum.at(:crypto.rand_uniform(0, length(formats)))
+    |> Faker.format
+  end
+
+  @doc """
+  Returns an app name.
   """
   @spec name() :: String.t
   sampler :name, ["Redhold", "Treeflex", "Trippledex", "Kanlam", "Bigtax", "Daltfresh", "Toughjoyfax", "Mat Lam Tam", "Otcom", "Tres-Zap", "Y-Solowarm", "Tresom", "Voltsillam", "Biodex", "Greenlam", "Viva", "Matsoft", "Temp", "Zoolab", "Subin", "Rank", "Job", "Stringtough", "Tin", "It", "Home Ing", "Zamit", "Sonsing", "Konklab", "Alpha", "Latlux", "Voyatouch", "Alphazap", "Holdlamis", "Zaam-Dox", "Sub-Ex", "Quo Lux", "Bamity", "Ventosanzap", "Lotstring", "Hatity", "Tempsoft", "Overhold", "Fixflex", "Konklux", "Zontrax", "Tampflex", "Span", "Namfix", "Transcof", "Stim", "Fix San", "Sonair", "Stronghold", "Fintone", "Y-find", "Opela", "Lotlux", "Ronstring", "Zathin", "Duobam", "Keylex"]
 
   @doc """
-  Return author name.
+  Returns an author name.
   """
   @spec author() :: String.t
   def author do
