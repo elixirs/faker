dce734c557e42318f2cb16877210f00aad982e16
Stefan Osorio <stefan@osorio.de>
2017-10-24 03:05:09 +0200

    Adapted date_of_birth(when range) to use new date_of_birth(when int)

--- lib/faker/date.ex
+++ lib/faker/date.ex
@@ -12,12 +12,11 @@ defmodule Faker.Date do
       date_of_birth(1) #=> ~D[2015-12-06]
       date_of_birth(10..19) #=> ~D[2004-05-15]
   """
-  @spec date_of_birth() :: Date.t
+  @spec date_of_birth() :: Date.t()
   def date_of_birth(age_or_range \\ 18..99)
 
-  @spec date_of_birth(integer) :: Date.t
+  @spec date_of_birth(integer) :: Date.t()
   def date_of_birth(age) when is_integer(age) do
-
     {{year_now, month_now, day_now}, _time} = :calendar.local_time()
 
     earliest_year = year_now - (age + 1)
@@ -37,31 +36,35 @@ defmodule Faker.Date do
         false -> {earliest_year + 1, 2, 28}
       end
 
-    earliest_as_seconds = :calendar.datetime_to_gregorian_seconds({earliest_date, {0,0,0}})
-    lastest_as_seconds = :calendar.datetime_to_gregorian_seconds({latest_date, {23,59,59}})
-    
+    earliest_as_seconds = :calendar.datetime_to_gregorian_seconds({earliest_date, {0, 0, 0}})
+    lastest_as_seconds = :calendar.datetime_to_gregorian_seconds({latest_date, {23, 59, 59}})
+
     {chosen_date, _time} =
       (lastest_as_seconds - earliest_as_seconds)
       |> :rand.uniform()
       |> Kernel.+(earliest_as_seconds)
-      |> :calendar.gregorian_seconds_to_datetime;
+      |> :calendar.gregorian_seconds_to_datetime()
 
     {:ok, result} = Date.from_erl(chosen_date)
     result
   end
 
-
-  @spec date_of_birth(Range.t) :: Date.t
+  @spec date_of_birth(Range.t()) :: Date.t()
   def date_of_birth(%Range{first: first, last: last}) do
-    {{current_year, current_month, current_day}, _time} = :calendar.local_time()
-    random_month = :crypto.rand_uniform(1, 13)
-    random_day = :crypto.rand_uniform(1, 29)
-    already_aged_this_year = current_month > random_month || current_month == random_month && random_day >= current_day
-    random_year = current_year - :crypto.rand_uniform(first, last) + (if already_aged_this_year, do: 1, else: 0)
-    {:ok, date} = Date.new random_year, random_month, random_day
-    date
-  end
+    random_age =
+      cond do
+        first < last ->
+          first + :rand.uniform(last - first + 1) - 1
 
+        first > last ->
+          last + :rand.uniform(first - last + 1) - 1
+
+        true ->
+          first
+      end
+
+    date_of_birth(random_age)
+  end
 
 
   @doc """
