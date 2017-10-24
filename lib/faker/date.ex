f156b119ef973ab9729a6a9e55d805b4f133f437
Stefan Osorio <stefan@osorio.de>
2017-10-24 02:40:26 +0200

    Rewrote Date.date_of_birth to cover more possible dates

--- lib/faker/date.ex
+++ lib/faker/date.ex
@@ -17,8 +17,39 @@ defmodule Faker.Date do
 
   @spec date_of_birth(integer) :: Date.t
   def date_of_birth(age) when is_integer(age) do
-    date_of_birth(%Range{first: age, last: age + 1})
+
+    {{year_now, month_now, day_now}, _time} = :calendar.local_time()
+
+    earliest_year = year_now - (age + 1)
+
+    potential_earliest_date = {earliest_year, month_now, day_now + 1}
+    potential_latest_date = {earliest_year + 1, month_now, day_now}
+
+    earliest_date =
+      case :calendar.valid_date(potential_earliest_date) do
+        true -> {earliest_year, month_now, day_now + 1}
+        false -> {earliest_year, 3, 1}
+      end
+
+    latest_date =
+      case :calendar.valid_date(potential_latest_date) do
+        true -> {earliest_year + 1, month_now, day_now}
+        false -> {earliest_year + 1, 2, 28}
+      end
+
+    earliest_as_seconds = :calendar.datetime_to_gregorian_seconds({earliest_date, {0,0,0}})
+    lastest_as_seconds = :calendar.datetime_to_gregorian_seconds({latest_date, {23,59,59}})
+    
+    {chosen_date, _time} =
+      (lastest_as_seconds - earliest_as_seconds)
+      |> :rand.uniform()
+      |> Kernel.+(earliest_as_seconds)
+      |> :calendar.gregorian_seconds_to_datetime;
+
+    {:ok, result} = Date.from_erl(chosen_date)
+    result
   end
+
 
   @spec date_of_birth(Range.t) :: Date.t
   def date_of_birth(%Range{first: first, last: last}) do
@@ -30,6 +61,8 @@ defmodule Faker.Date do
     {:ok, date} = Date.new random_year, random_month, random_day
     date
   end
+
+
 
   @doc """
   Returns a random date in the past up to N days, today not included
