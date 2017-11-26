4f082169d1bbafa9fc0edd19d24e77fad8db6614
Marcus Vieira <marcusbrunovieira@gmail.com>
2017-11-26 13:13:42 -0200

    Move `cat.md` examples to `cat.ex`

--- lib/faker/cat.ex
+++ lib/faker/cat.ex
@@ -5,25 +5,40 @@ defmodule Faker.Cat do
 
   @doc """
   Returns a ramdom Cat name
+
+  ## Examples
+
+      iex> Faker.Cat.name()
+      #=> "Shadow"
   """
-  @spec name() :: String.t
+  @spec name() :: String.t()
   def name do
-    Module.concat(__MODULE__, Faker.mlocale).name
+    Module.concat(__MODULE__, Faker.mlocale()).name
   end
 
   @doc """
   Returns a random Cat breed
+
+  ## Examples
+
+      iex> Faker.Cat.breed()
+      #=> "British Semipi-longhair"
   """
-  @spec breed() :: String.t
+  @spec breed() :: String.t()
   def breed do
-    Module.concat(__MODULE__, Faker.mlocale).breed
+    Module.concat(__MODULE__, Faker.mlocale()).breed
   end
 
   @doc """
   Returns a random Cat registry
+
+  ## Examples
+
+      iex> Faker.Cat.registry()
+      #=> "American Cat Fanciers Association"
   """
-  @spec registry() :: String.t
+  @spec registry() :: String.t()
   def registry do
-    Module.concat(__MODULE__, Faker.mlocale).registry
+    Module.concat(__MODULE__, Faker.mlocale()).registry
   end
 end
