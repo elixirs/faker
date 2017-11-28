7b5659338c2a69a423b7049bc616fefee7857057
Marcus Vieira <marcusbrunovieira@gmail.com>
2017-11-27 22:05:15 -0200

    Move `commerce.md` examples to `commerce.ex`

--- lib/faker/commerce.ex
+++ lib/faker/commerce.ex
@@ -5,22 +5,37 @@ defmodule Faker.Commerce do
 
   @doc """
   Returns a random color
+
+  ## Examples
+
+      iex> Faker.Commerce.color()
+      #=> "maroon"
   """
-  @spec color() :: String.t
+  @spec color() :: String.t()
   def color do
-    Module.concat(__MODULE__, Faker.mlocale).color
+    Module.concat(__MODULE__, Faker.mlocale()).color
   end
 
   @doc """
   Returns a random department
+
+  ## Examples
+
+      iex> Faker.Commerce.department()
+      #=> "Sports & Outdoors"
   """
-  @spec department() :: String.t
+  @spec department() :: String.t()
   def department do
-    Module.concat(__MODULE__, Faker.mlocale).department
+    Module.concat(__MODULE__, Faker.mlocale()).department
   end
 
   @doc """
   Returns a random number that represents a price
+
+  ## Examples
+
+      iex> Faker.Commerce.price()
+      #=> "3.5"
   """
   @spec price() :: float
   def price do
@@ -30,33 +45,53 @@ defmodule Faker.Commerce do
   @doc """
   Returns a complete product name, based on product adjectives, product
   materials, product names
+
+  ## Examples
+
+      iex> Faker.Commerce.product_name()
+      #=> "Fantastic Chair"
   """
-  @spec product_name() :: String.t
+  @spec product_name() :: String.t()
   def product_name do
-    Module.concat(__MODULE__, Faker.mlocale).product_name
+    Module.concat(__MODULE__, Faker.mlocale()).product_name
   end
 
   @doc """
   Returns a random adjective for a product
+
+  ## Examples
+
+      iex> Faker.Commerce.product_name_adjective()
+      #=> "Incredible"
   """
-  @spec product_name_adjective() :: String.t
+  @spec product_name_adjective() :: String.t()
   def product_name_adjective do
-    Module.concat(__MODULE__, Faker.mlocale).product_name_adjective
+    Module.concat(__MODULE__, Faker.mlocale()).product_name_adjective
   end
 
   @doc """
   Returns a random product material
+
+  ## Examples
+
+      iex> Faker.Commerce.product_name_material()
+      #=> "Wooden"
   """
-  @spec product_name_material() :: String.t
+  @spec product_name_material() :: String.t()
   def product_name_material do
-    Module.concat(__MODULE__, Faker.mlocale).product_name_material
+    Module.concat(__MODULE__, Faker.mlocale()).product_name_material
   end
 
   @doc """
   Returns a random product name
+
+  ## Examples
+
+      iex> Faker.Commerce.product_name_product()
+      #=> "Gloves"
   """
-  @spec product_name_product() :: String.t
+  @spec product_name_product() :: String.t()
   def product_name_product do
-    Module.concat(__MODULE__, Faker.mlocale).product_name_product
+    Module.concat(__MODULE__, Faker.mlocale()).product_name_product
   end
 end
