c916d6bc2d1519ebaa787d5ca84d073202036598
Vitor Oliveira <vbrazo@gmail.com>
2018-05-03 04:56:02 -0300

    Add translation internet - Spanish and Brazilian Portuguese

---
+++ lib/faker/internet/es.ex
@@ -0,0 +1,19 @@
+defmodule Faker.Internet.Es do
+  import Faker, only: [sampler: 2]
+
+  @moduledoc"""
+  Generating internet related data in Spanish
+  """
+
+  @doc """
+  Returns a random free email service name
+  """
+  @spec free_email_service() :: String
+  sampler :free_email_service, ["gmail.com", "yahoo.com", "hotmail.com"]
+
+  @doc """
+  Returns a random domain suffix
+  """
+  @spec domain_suffix() :: String.t
+  sampler :domain_suffix, ["com", "es", "info", "com.es", "org"]
+end
---
+++ lib/faker/internet/pt_br.ex
@@ -0,0 +1,19 @@
+defmodule Faker.Internet.PtBr do
+  import Faker, only: [sampler: 2]
+
+  @moduledoc"""
+  Generating internet related data in Brazilian Portuguese
+  """
+
+  @doc """
+  Returns a random free email service name
+  """
+  @spec free_email_service() :: String
+  sampler :free_email_service, ["gmail.com", "yahoo.com", "hotmail.com", "live.com", "bol.com.br"]
+
+  @doc """
+  Returns a random domain suffix
+  """
+  @spec domain_suffix() :: String.t
+  sampler :domain_suffix, ["br", "biz", "info", "name", "net", "org"]
+end
