6adc324daa0dc1e6c7709617971968002bd11018
Vitor Oliveira <vbrazo@gmail.com>
2018-06-16 19:02:58 -0300

    Add doctests for cat

--- test/faker/cat_test.exs
+++ test/faker/cat_test.exs
@@ -1,15 +1,5 @@
-defmodule CatTest do
+defmodule Faker.Cat.EnTest do
   use ExUnit.Case, async: true
 
-  test "name/0" do
-    assert is_binary(Faker.Cat.name)
-  end
-
-  test "breed/0" do
-    assert is_binary(Faker.Cat.breed)
-  end
-
-  test "registry/0" do
-    assert is_binary(Faker.Cat.registry)
-  end
+  doctest Faker.Cat.En
 end
