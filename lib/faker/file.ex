e27617652ee83fa3d91f9e077713fc29f5a1a6ac
Igor Kapkov <igasgeek@me.com>
2018-06-19 21:20:15 +1000

    Added: doctests for File

--- lib/faker/file.ex
+++ lib/faker/file.ex
@@ -14,14 +14,20 @@ defmodule Faker.File do
   }
 
   @mimes %{
-    application: ~w(application/atom+xml application/ecmascript application/EDI-X12 application/EDIFACT application/json application/javascript application/ogg application/pdf application/postscript application/rdf+xml application/rss+xml application/soap+xml application/font-woff application/xhtml+xml application/xml-dtd application/xop+xml application/zip application/gzip),
-    audio: ~w(audio/basic audio/L24 audio/mp4 audio/mpeg audio/ogg audio/vorbis audio/vnd.rn-realaudio audio/vnd.wave audio/webm),
-    image: ~w(image/gif image/jpeg image/pjpeg image/png image/svg+xml image/tiff image/vnd.microsoft.icon),
+    application:
+      ~w(application/atom+xml application/ecmascript application/EDI-X12 application/EDIFACT application/json application/javascript application/ogg application/pdf application/postscript application/rdf+xml application/rss+xml application/soap+xml application/font-woff application/xhtml+xml application/xml-dtd application/xop+xml application/zip application/gzip),
+    audio:
+      ~w(audio/basic audio/L24 audio/mp4 audio/mpeg audio/ogg audio/vorbis audio/vnd.rn-realaudio audio/vnd.wave audio/webm),
+    image:
+      ~w(image/gif image/jpeg image/pjpeg image/png image/svg+xml image/tiff image/vnd.microsoft.icon),
     message: ~w(message/http message/imdn+xml message/partial message/rfc822),
-    model: ~w(model/example model/iges model/mesh model/vrml model/x3d+binary model/x3d+vrml model/x3d+xml),
-    multipart: ~w(multipart/mixed multipart/alternative multipart/related multipart/form-data multipart/signed multipart/encrypted),
+    model:
+      ~w(model/example model/iges model/mesh model/vrml model/x3d+binary model/x3d+vrml model/x3d+xml),
+    multipart:
+      ~w(multipart/mixed multipart/alternative multipart/related multipart/form-data multipart/signed multipart/encrypted),
     text: ~w(text/cmd text/css text/csv text/html text/javascript text/plain text/vcard text/xml),
-    video: ~w(video/mpeg video/mp4 video/ogg video/quicktime video/webm video/x-matroska video/x-ms-wmv video/x-flv)
+    video:
+      ~w(video/mpeg video/mp4 video/ogg video/quicktime video/webm video/x-matroska video/x-ms-wmv video/x-flv)
   }
 
   @categories_extensions Map.keys(@extensions)
@@ -33,9 +39,15 @@ defmodule Faker.File do
   ## Examples
 
       iex> Faker.File.file_extension()
-      #=> "mp3"
+      "wav"
+      iex> Faker.File.file_extension()
+      "wav"
+      iex> Faker.File.file_extension()
+      "doc"
+      iex> Faker.File.file_extension()
+      "mov"
   """
-  @spec file_extension() :: String.t
+  @spec file_extension() :: String.t()
   def file_extension do
     @categories_extensions
     |> pick
@@ -49,9 +61,15 @@ defmodule Faker.File do
   ## Examples
 
       iex> Faker.File.file_extension(:video)
-      #=> "webm"
+      "mov"
+      iex> Faker.File.file_extension(:image)
+      "tiff"
+      iex> Faker.File.file_extension(:audio)
+      "flac"
+      iex> Faker.File.file_extension(:office)
+      "xls"
   """
-  @spec file_extension(:atom) :: String.t
+  @spec file_extension(:atom) :: String.t()
   def file_extension(category) do
     category
     |> get_extensions_from_category()
@@ -64,11 +82,17 @@ defmodule Faker.File do
   ## Examples
 
       iex> Faker.File.file_name()
-      #=> "voluptaes.jpg"
+      "aliquam.jpg"
+      iex> Faker.File.file_name()
+      "deleniti.doc"
+      iex> Faker.File.file_name()
+      "qui.jpg"
+      iex> Faker.File.file_name()
+      "quibusdam.csv"
   """
-  @spec file_name() :: String.t
+  @spec file_name() :: String.t()
   def file_name do
-    Lorem.word <> "." <> file_extension()
+    Lorem.word() <> "." <> file_extension()
   end
 
   @doc """
@@ -78,11 +102,17 @@ defmodule Faker.File do
   ## Examples
 
       iex> Faker.File.file_name(:text)
-      #=> "reo.json"
+      "aliquam.txt"
+      iex> Faker.File.file_name(:video)
+      "sint.mp4"
+      iex> Faker.File.file_name(:image)
+      "consequatur.bmp"
+      iex> Faker.File.file_name(:audio)
+      "qui.wav"
   """
-  @spec file_name(:atom) :: String.t
+  @spec file_name(:atom) :: String.t()
   def file_name(category) do
-    Lorem.word <> "." <> file_extension(category)
+    Lorem.word() <> "." <> file_extension(category)
   end
 
   @doc """
@@ -91,9 +121,15 @@ defmodule Faker.File do
   ## Examples
 
       iex> Faker.File.mime_type()
-      #=> "application/atom+xml"
+      "text/css"
+      iex> Faker.File.mime_type()
+      "message/http"
+      iex> Faker.File.mime_type()
+      "application/ogg"
+      iex> Faker.File.mime_type()
+      "model/x3d+xml"
   """
-  @spec mime_type :: String.t
+  @spec mime_type :: String.t()
   def mime_type do
     @categories_mimes
     |> pick
@@ -108,9 +144,15 @@ defmodule Faker.File do
   ## Examples
 
       iex> Faker.File.mime_type(:image)
-      #=> "image/gif"
+      "image/vnd.microsoft.icon"
+      iex> Faker.File.mime_type(:audio)
+      "audio/mp4"
+      iex> Faker.File.mime_type(:application)
+      "application/xop+xml"
+      iex> Faker.File.mime_type(:video)
+      "video/mpeg"
   """
-  @spec mime_type(:atom) :: String.t
+  @spec mime_type(:atom) :: String.t()
   def mime_type(category) do
     category
     |> get_mimes_from_category()
