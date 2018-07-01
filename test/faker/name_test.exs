e53964795f2d76ef2f15947cad3df265267a8e13
Vitor Oliveira <vbrazo@gmail.com>
2018-07-01 20:50:57 -0300

    Changes after reviews

--- test/faker/name_test.exs
+++ test/faker/name_test.exs
@@ -1,31 +1,8 @@
 defmodule Faker.NameTest do
   use ExUnit.Case, async: true
 
+  doctest Faker.Name
   doctest Faker.Name.En
   doctest Faker.Name.Es
   doctest Faker.Name.PtBr
-
-  test "first_name/0" do
-    assert is_binary(Faker.Name.first_name())
-  end
-
-  test "last_name/0" do
-    assert is_binary(Faker.Name.last_name())
-  end
-
-  test "name/0" do
-    assert is_binary(Faker.Name.name())
-  end
-
-  test "prefix/0" do
-    assert is_binary(Faker.Name.prefix())
-  end
-
-  test "suffix/0" do
-    assert is_binary(Faker.Name.suffix())
-  end
-
-  test "title/0" do
-    assert is_binary(Faker.Name.title())
-  end
 end
