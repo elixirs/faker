ad223a26fdb00949b37fb14c199fdda485eb183c
Vitor Oliveira <vbrazo@gmail.com>
2018-07-04 10:31:43 -0300

    Remove old is_binary asserts

--- test/faker/internet_test.exs
+++ test/faker/internet_test.exs
@@ -7,41 +7,34 @@ defmodule InternetTest do
   doctest Faker.Internet.PtBr
 
   test "ip_v4_address/0" do
-    assert is_binary(Faker.Internet.ip_v4_address())
     assert Regex.match?(~r/^(\d+\.){3}\d+$/, Faker.Internet.ip_v4_address())
   end
 
   test "ip_v6_address/0" do
-    assert is_binary(Faker.Internet.ip_v6_address())
     assert Regex.match?(~r/^([0-9A-F]{4}:){7}[0-9A-F]{4}$/, Faker.Internet.ip_v6_address())
   end
 
   test "mac_address/0" do
-    assert is_binary(Faker.Internet.mac_address())
     assert Regex.match?(~r/^([0-9a-f]{2}:){5}[0-9a-f]{2}$/, Faker.Internet.mac_address())
   end
 
   test "url/0" do
-    assert is_binary(Faker.Internet.url())
     assert Regex.match?(~r/^http[s]?:\/\/\w+\.\w+$/, Faker.Internet.url())
   end
 
   test "slug/0" do
-    assert is_binary(Faker.Internet.slug())
     assert Regex.match?(~r/\A([a-z0-9]+[-\._])*[a-z0-9]+\z/, Faker.Internet.slug())
   end
 
   test "slug/1" do
     slug = Faker.Internet.slug(["hello", "world"])
 
-    assert is_binary(slug)
     assert Regex.match?(~r/\A((hello|world)[-\._])(hello|world)\z/, slug)
   end
 
   test "slug/2" do
     slug = Faker.Internet.slug(["hello", "world"], ["-"])
 
-    assert is_binary(slug)
     assert Regex.match?(~r/\A((hello|world)-)(hello|world)\z/, slug)
   end
 end
