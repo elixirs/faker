e296d4da7e7ed79161db4ec94b1655320850e0ef
Igor Kapkov <igasgeek@me.com>
2018-07-07 17:30:29 +1000

    Added: Faker.Internet.UserAgent doctests

--- test/faker/internet/user_agent_test.exs
+++ test/faker/internet/user_agent_test.exs
@@ -1,37 +1,5 @@
-defmodule Internet.UserAgentTest do
+defmodule Faker.Internet.UserAgentTest do
   use ExUnit.Case, async: true
 
-  alias Faker.Internet.UserAgent
-
-  test "user_agent/0" do
-    assert is_binary(UserAgent.user_agent)
-  end
-
-  test "bot_user_agent/0" do
-    assert is_binary(UserAgent.bot_user_agent)
-  end
-
-  test "desktop_user_agent/0" do
-    assert is_binary(UserAgent.desktop_user_agent)
-  end
-
-  test "ereader_user_agent/0" do
-    assert is_binary(UserAgent.ereader_user_agent)
-  end
-
-  test "game_console_user_agent/0" do
-    assert is_binary(UserAgent.game_console_user_agent)
-  end
-
-  test "set_top_user_agent/0" do
-    assert is_binary(UserAgent.set_top_user_agent)
-  end
-
-  test "tablet_user_agent/0" do
-    assert is_binary(UserAgent.tablet_user_agent)
-  end
-
-  test "mobile_user_agent/0" do
-    assert is_binary(UserAgent.mobile_user_agent)
-  end
+  doctest Faker.Internet.UserAgent
 end
