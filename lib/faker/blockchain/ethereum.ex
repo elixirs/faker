61a68460612f4a91b389e38c6c903a8f2a0711dc
Jean Parpaillon <jean.parpaillon@free.fr>
2019-07-08 10:13:41 +0200

    * Add 4 test cases for ethereum * Fix ethereum address format

--- lib/faker/blockchain/ethereum.ex
+++ lib/faker/blockchain/ethereum.ex
@@ -10,13 +10,20 @@ defmodule Faker.Blockchain.Ethereum do
 
   ## Examples
 
-    iex> Faker.Blockchain.Ethereum.address()
-    
+      iex> Faker.Blockchain.Ethereum.address()
+      "0xd6d98b88c866f496dbd4de7ba48d0f5229fa7bf9"
+      iex> Faker.Blockchain.Ethereum.address()
+      "0x0728b27267bc5b7c964f332dc9edd02cc9f381de"
+      iex> Faker.Blockchain.Ethereum.address()
+      "0xf9d922a146bf85508a5f03ff18750bf363f4aef1"
+      iex> Faker.Blockchain.Ethereum.address()
+      "0x264e3bcc9b5c2accb99a3a4993ad56b778dc26ed"
   """
   @spec address() :: address
   def address do
-    20
-    |> Faker.random_bytes()
-    |> Base.encode16(case: :lower)
+    "0x" <>
+      (20
+       |> Faker.random_bytes()
+       |> Base.encode16(case: :lower))
   end
 end
