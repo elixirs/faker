aefa58849e8a8b9b4dcf05218d54e87d64d5052c
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-08-31 07:15:47 -0400

    Fixed issue with Date.date_of_birth/1 having the potential of using an invalid date range on the last day of the month

--- lib/faker/date.ex
+++ lib/faker/date.ex
@@ -12,33 +12,20 @@ defmodule Faker.Date do
   def date_of_birth(age_or_range \\ 18..99)
 
   def date_of_birth(age) when is_integer(age) do
-    {{year_now, month_now, day_now}, _time} = :calendar.local_time()
+    {date, _time} = :calendar.local_time()
 
-    earliest_year = year_now - (age + 1)
+    today = Date.from_erl!(date)
 
-    potential_earliest_date = {earliest_year, month_now, day_now + 1}
-    potential_latest_date = {earliest_year + 1, month_now, day_now}
+    earliest_year = today.year - (age + 1)
 
-    earliest_date =
-      if :calendar.valid_date(potential_earliest_date),
-        do: {earliest_year, month_now, day_now + 1},
-        else: {earliest_year, 3, 1}
+    {:ok, earliest_date} = Date.new(earliest_year, today.month, today.day)
+    earliest_date = Date.add(earliest_date, 1)
 
-    latest_date =
-      if :calendar.valid_date(potential_latest_date),
-        do: {earliest_year + 1, month_now, day_now},
-        else: {earliest_year + 1, 2, 28}
+    {:ok, latest_date} = Date.new(earliest_year + 1, today.month, today.day)
 
-    earliest_as_seconds = :calendar.datetime_to_gregorian_seconds({earliest_date, {0, 0, 0}})
-    lastest_as_seconds = :calendar.datetime_to_gregorian_seconds({latest_date, {23, 59, 59}})
+    date_range = Date.range(earliest_date, latest_date)
 
-    {chosen_date, _time} =
-      earliest_as_seconds..lastest_as_seconds
-      |> pick()
-      |> :calendar.gregorian_seconds_to_datetime()
-
-    {:ok, result} = Date.from_erl(chosen_date)
-    result
+    pick(date_range)
   end
 
   def date_of_birth(age_range) do
