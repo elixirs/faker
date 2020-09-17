6c18357837c1712feb71f4475f26508c7cf0a863
Vincent Boivin <vboivin04@gmail.com>
2020-09-17 14:20:20 -0400

    Added region name in french

---
+++ lib/faker/aws/fr.ex
@@ -0,0 +1,44 @@
+defmodule Faker.AWS.Fr do
+  import Faker, only: [sampler: 2]
+
+  @moduledoc """
+  Functions for generating AWS information in French
+  """
+
+  @doc """
+  Returns a random region name available on AWS in French
+
+  ## Examples
+  iex> Faker.Aws.En.region_name()
+  "Asie Pacifique (Tokyo)"
+  iex> Faker.Aws.En.region_name()
+  "US East (Ohio)"
+  iex> Faker.Aws.En.region_name()
+  "Europe (Milan)"
+  iex> Faker.Aws.En.region_name()
+  "Africa (Cape Town)"
+  """
+  @spec region_name():: String.t()
+  sampler(:region_name, [
+    "USA Est (Virginie du Nord)",
+    "USA Est (Ohio)",
+    "USA Ouest (Californie du Nord)",
+    "USA Ouest (Oregon)",
+    "Afrique (Le Cap)",
+    "Asie Pacifique (Hong Kong)",
+    "Asie Pacifique (Mumbai)",
+    "Asie Pacifique (Seoul)",
+    "Asie Pacifique (Singapore)",
+    "Asie Pacifique (Sydney)",
+    "Asie Pacifique (Tokyo)",
+    "Canada (Central)",
+    "Europe (Frankfurt)",
+    "Europe (Ireland)",
+    "Europe (London)",
+    "Europe (Milan)",
+    "Europe (Paris)",
+    "Europe (Stockholm)",
+    "Moyen-Orient (Bahreïn)",
+    "Amérique du Sud (São Paulo)"
+  ])
+end
