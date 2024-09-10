477a5d7ab5e4ac88f521dc12d720852fc1fdccdd
Marcus Wood <github@igas.me>
2024-09-10 22:30:02 +1000

    Discard changes to lib/faker/commerce.ex

--- lib/faker/commerce.ex
+++ lib/faker/commerce.ex
@@ -11,7 +11,13 @@ defmodule Faker.Commerce do
   ## Examples
 
       iex> Faker.Commerce.color()
-      "turquoise"
+      "red"
+      iex> Faker.Commerce.color()
+      "sky blue"
+      iex> Faker.Commerce.color()
+      "lavender"
+      iex> Faker.Commerce.color()
+      "grey"
   """
   @spec color() :: String.t()
   localize(:color)
@@ -23,6 +29,12 @@ defmodule Faker.Commerce do
 
       iex> Faker.Commerce.department()
       "Home, Garden & Tools"
+      iex> Faker.Commerce.department()
+      "Electronics & Computers"
+      iex> Faker.Commerce.department()
+      "Clothing, Shoes & Jewelery"
+      iex> Faker.Commerce.department()
+      "Toys, Kids & Baby"
   """
   @spec department() :: String.t()
   localize(:department)
@@ -33,7 +45,13 @@ defmodule Faker.Commerce do
   ## Examples
 
       iex> Faker.Commerce.price()
-      0.61
+      1.11
+      iex> Faker.Commerce.price()
+      4.02
+      iex> Faker.Commerce.price()
+      8.36
+      iex> Faker.Commerce.price()
+      3.05
   """
   @spec price() :: float
   def price do
@@ -48,6 +66,12 @@ defmodule Faker.Commerce do
 
       iex> Faker.Commerce.product_name()
       "Ergonomic Steel Shirt"
+      iex> Faker.Commerce.product_name()
+      "Fantastic Car"
+      iex> Faker.Commerce.product_name()
+      "Granite Gloves"
+      iex> Faker.Commerce.product_name()
+      "Plastic Shoes"
   """
   @spec product_name() :: String.t()
   localize(:product_name)
@@ -59,6 +83,12 @@ defmodule Faker.Commerce do
 
       iex> Faker.Commerce.product_name_adjective()
       "Small"
+      iex> Faker.Commerce.product_name_adjective()
+      "Ergonomic"
+      iex> Faker.Commerce.product_name_adjective()
+      "Incredible"
+      iex> Faker.Commerce.product_name_adjective()
+      "Gorgeous"
   """
   @spec product_name_adjective() :: String.t()
   localize(:product_name_adjective)
@@ -70,6 +100,12 @@ defmodule Faker.Commerce do
 
       iex> Faker.Commerce.product_name_material()
       "Rubber"
+      iex> Faker.Commerce.product_name_material()
+      "Concrete"
+      iex> Faker.Commerce.product_name_material()
+      "Steel"
+      iex> Faker.Commerce.product_name_material()
+      "Granite"
   """
   @spec product_name_material() :: String.t()
   localize(:product_name_material)
@@ -80,7 +116,13 @@ defmodule Faker.Commerce do
   ## Examples
 
       iex> Faker.Commerce.product_name_product()
-      "Shoes"
+      "Gloves"
+      iex> Faker.Commerce.product_name_product()
+      "Computer"
+      iex> Faker.Commerce.product_name_product()
+      "Table"
+      iex> Faker.Commerce.product_name_product()
+      "Shirt"
   """
   @spec product_name_product() :: String.t()
   localize(:product_name_product)
