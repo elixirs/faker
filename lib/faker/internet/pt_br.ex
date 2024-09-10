04ec155be0f17543d0dfe454ed7b0e5360755201
Marcus Wood <github@igas.me>
2024-09-10 22:29:18 +1000

    Discard changes to lib/faker/internet/pt_br.ex

--- lib/faker/internet/pt_br.ex
+++ lib/faker/internet/pt_br.ex
@@ -12,6 +12,12 @@ defmodule Faker.Internet.PtBr do
 
       iex> Faker.Internet.PtBr.free_email_service()
       "gmail.com"
+      iex> Faker.Internet.PtBr.free_email_service()
+      "yahoo.com"
+      iex> Faker.Internet.PtBr.free_email_service()
+      "gmail.com"
+      iex> Faker.Internet.PtBr.free_email_service()
+      "bol.com.br"
   """
   @spec free_email_service() :: String
   sampler(:free_email_service, ["gmail.com", "yahoo.com", "hotmail.com", "live.com", "bol.com.br"])
@@ -23,6 +29,12 @@ defmodule Faker.Internet.PtBr do
 
       iex> Faker.Internet.PtBr.domain_suffix()
       "br"
+      iex> Faker.Internet.PtBr.domain_suffix()
+      "org"
+      iex> Faker.Internet.PtBr.domain_suffix()
+      "name"
+      iex> Faker.Internet.PtBr.domain_suffix()
+      "info"
   """
   @spec domain_suffix() :: String.t()
   sampler(:domain_suffix, ["br", "biz", "info", "name", "net", "org"])
