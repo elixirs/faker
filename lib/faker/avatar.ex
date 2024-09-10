79372810c5e90d8e15c8d8fbafff1028b55f74b2
Marcus Wood <github@igas.me>
2024-09-10 13:57:20 +1000

    Discard changes to lib/faker/avatar.ex

--- lib/faker/avatar.ex
+++ lib/faker/avatar.ex
@@ -12,6 +12,12 @@ defmodule Faker.Avatar do
 
       iex> Faker.Avatar.image_url()
       "https://robohash.org/set_set1/bgset_bg2/kQqaIfGqxsjFoNIT"
+      iex> Faker.Avatar.image_url()
+      "https://robohash.org/set_set2/bgset_bg2/6"
+      iex> Faker.Avatar.image_url()
+      "https://robohash.org/set_set2/bgset_bg2/J"
+      iex> Faker.Avatar.image_url()
+      "https://robohash.org/set_set3/bgset_bg1/JNth88PrhGDhwp4LNQMt"
   """
   @spec image_url() :: String.t()
   def image_url do
@@ -27,6 +33,10 @@ defmodule Faker.Avatar do
       "https://robohash.org/faker"
       iex> Faker.Avatar.image_url("elixir")
       "https://robohash.org/elixir"
+      iex> Faker.Avatar.image_url("plug")
+      "https://robohash.org/plug"
+      iex> Faker.Avatar.image_url("ecto")
+      "https://robohash.org/ecto"
   """
   @spec image_url(binary) :: String.t()
   def image_url(slug) do
@@ -40,9 +50,13 @@ defmodule Faker.Avatar do
   ## Examples
 
       iex> Faker.Avatar.image_url(200, 200)
-      "https://robohash.org/set_set3/bgset_bg2/oQAJDfv8?size=200x200"
+      "https://robohash.org/set_set2/bgset_bg2/ppkQqaIfGqx?size=200x200"
       iex> Faker.Avatar.image_url(800, 600)
-      "https://robohash.org/set_set2/bgset_bg2/zi6hv78yuUKwc1jeSsL?size=800x600"
+      "https://robohash.org/set_set2/bgset_bg2/oNITNnu6?size=800x600"
+      iex> Faker.Avatar.image_url(32, 32)
+      "https://robohash.org/set_set3/bgset_bg1/J?size=32x32"
+      iex> Faker.Avatar.image_url(128, 128)
+      "https://robohash.org/set_set1/bgset_bg2/JNth88PrhGDhwp4LNQMt?size=128x128"
   """
   @spec image_url(integer, integer) :: String.t()
   def image_url(width, height)
@@ -60,6 +74,10 @@ defmodule Faker.Avatar do
       "https://robohash.org/phoenix?size=100x100"
       iex> Faker.Avatar.image_url("haskell", 200, 200)
       "https://robohash.org/haskell?size=200x200"
+      iex> Faker.Avatar.image_url("ocaml", 300, 300)
+      "https://robohash.org/ocaml?size=300x300"
+      iex> Faker.Avatar.image_url("idris", 400, 400)
+      "https://robohash.org/idris?size=400x400"
   """
   @spec image_url(binary, integer, integer) :: String.t()
   def image_url(slug, width, height)
