35c0ce2a64bea486e4c12fbd347ff5e43ff4e5f2
Marcus Wood <github@igas.me>
2025-01-30 22:27:41 +1100

    Discard changes to lib/faker/commerce/hy.ex

--- lib/faker/commerce/hy.ex
+++ lib/faker/commerce/hy.ex
@@ -13,6 +13,12 @@ defmodule Faker.Commerce.Hy do
 
       iex> Faker.Commerce.Hy.color()
       "մոխրագույն"
+      iex> Faker.Commerce.Hy.color()
+      "կանաչ"
+      iex> Faker.Commerce.Hy.color()
+      "երկնագույն"
+      iex> Faker.Commerce.Hy.color()
+      "մանուշակագույն"
   """
   @spec color() :: String.t()
   def color do
@@ -26,6 +32,12 @@ defmodule Faker.Commerce.Hy do
 
       iex> Faker.Commerce.Hy.department()
       "Համակարգիչներ"
+      iex> Faker.Commerce.Hy.department()
+      "Երաժշտություն"
+      iex> Faker.Commerce.Hy.department()
+      "Գրքեր"
+      iex> Faker.Commerce.Hy.department()
+      "Էլեկտրոնիկա"
   """
   @spec department() :: String.t()
   sampler(:department, [
@@ -60,6 +72,12 @@ defmodule Faker.Commerce.Hy do
 
       iex> Faker.Commerce.Hy.product_name()
       "հիանալի բրոնզե գլխարկ"
+      iex> Faker.Commerce.Hy.product_name()
+      "ֆանտաստիկ դանակ"
+      iex> Faker.Commerce.Hy.product_name()
+      "պլաստիկից աթոռ"
+      iex> Faker.Commerce.Hy.product_name()
+      "ալյումինե վերնաշապիկ"
   """
   @spec product_name() :: String.t()
   def product_name, do: product_name(Faker.random_between(0, 2))
@@ -78,6 +96,12 @@ defmodule Faker.Commerce.Hy do
 
       iex> Faker.Commerce.Hy.product_name_adjective()
       "ֆանտաստիկ"
+      iex> Faker.Commerce.Hy.product_name_adjective()
+      "հիանալի"
+      iex> Faker.Commerce.Hy.product_name_adjective()
+      "միջակ"
+      iex> Faker.Commerce.Hy.product_name_adjective()
+      "նրբագեղ"
   """
   @spec product_name_adjective() :: String.t()
   sampler(:product_name_adjective, [
@@ -106,6 +130,12 @@ defmodule Faker.Commerce.Hy do
 
       iex> Faker.Commerce.Hy.product_name_material()
       "փայտե"
+      iex> Faker.Commerce.Hy.product_name_material()
+      "գրանիտե"
+      iex> Faker.Commerce.Hy.product_name_material()
+      "բրոնզե"
+      iex> Faker.Commerce.Hy.product_name_material()
+      "մարմարե"
   """
   @spec product_name_material() :: String.t()
   sampler(:product_name_material, [
@@ -135,6 +165,12 @@ defmodule Faker.Commerce.Hy do
 
       iex> Faker.Commerce.Hy.product_name_product()
       "վերնաշապիկ"
+      iex> Faker.Commerce.Hy.product_name_product()
+      "ստեղնաշար"
+      iex> Faker.Commerce.Hy.product_name_product()
+      "վերնաշապիկ"
+      iex> Faker.Commerce.Hy.product_name_product()
+      "գլխարկ"
   """
   @spec product_name_product() :: String.t()
   sampler(:product_name_product, [
