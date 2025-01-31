3f8cbc143184344d09ef53e1740eff0c49379181
Marcus Wood <github@igas.me>
2025-01-31 23:20:00 +1100

    fix: makeup packages (#592)

--- mix.exs
+++ mix.exs
@@ -46,11 +46,13 @@ defmodule Faker.Mixfile do
 
   defp deps do
     [
-      {:ex_doc, "== 0.36.1", only: :dev, runtime: false},
-      {:earmark, "== 1.4.47", only: :dev, runtime: false},
       {:credo, "== 1.7.11", only: [:dev, :test], runtime: false},
       {:dialyxir, "== 1.4.5", only: [:dev], runtime: false},
-      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
+      {:earmark, "== 1.4.47", only: :dev, runtime: false},
+      {:ex_doc, "== 0.36.1", only: :dev, runtime: false},
+      {:makeup, "== 1.2.1"},
+      {:makeup_elixir, "== 1.0.1"},
+      {:mix_test_watch, "== 1.2.0", only: [:dev, :test], runtime: false}
     ]
   end
 
